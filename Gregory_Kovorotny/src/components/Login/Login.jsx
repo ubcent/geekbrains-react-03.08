ba536656d5c6bf3e0be3c9881ad535cb1452c508
greforce <greforce76@gmail.com>
2018-08-12 14:16:48 +0300

    lesson3 - validation trial of Inputs

--- Gregory_Kovorotny/src/components/Login/Login.jsx
+++ Gregory_Kovorotny/src/components/Login/Login.jsx
@@ -8,18 +8,27 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-  ModalFooter } from 'reactstrap';
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormFeedback } from 'reactstrap';
 
-import LoginForm from '../LoginForm';
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      userLogin: '',
+      userPassword: '',
+      userLoginError: false,
+      userPasswordError: false,
     };
 
     this.toggle = this.toggle.bind(this);
+    this.login = this.login.bind(this);
   }
 
   toggle() {
@@ -28,17 +37,57 @@ export default class Login extends Component {
     });
   }
 
+  login() {
+    console.log('Login pressed');
+    const { userLogin, userPassword } = this.state;
+    console.log(this.state); // тут modal пока что === true. это ясно
+
+    if (userLogin === 'user' && userPassword === '1234') {
+      this.setState({ userLoginError: false });
+      this.setState({ userPasswordError: false });
+      this.toggle();
+      console.log(this.state); // почему modal все еще === true???
+      console.log(this); // а вот тут все ОК: modal === false !!!
+    } else {
+      this.setState({ userLoginError: true });
+      this.setState({ userPasswordError: true });
+    }
+  }
+
   render() {
     const className = 'login';
     return (
-      // Buttons лучше оставить в Modal? или перенести в Form (LoginForm.jsx компонент)
-      // но в этом случае не нашел пока как сделать, чтобы по Enter срабатывала Login Button
+      // решил не выделять LoginForm в отдельный компонент
+      // чтобы проще было работать с Input-ами
       <div className={className}>
         <Button outline color="secondary" onClick={this.toggle}>Login</Button>
         <Modal autoFocus={false} centered isOpen={this.state.modal} toggle={this.toggle} className={className}>
-          <ModalBody><LoginForm /></ModalBody>
+          <ModalBody>
+            <div className='login-form'>
+              <Form>
+                <FormGroup>
+                  <Label for='userLogin'>Login</Label>
+                  <Input
+                    autoFocus type='text' name='userLogin' id='userLogin' placeholder='Your login'
+                    invalid={this.state.userLoginError}
+                    onChange={(e) => this.setState({ userLogin: e.target.value })}
+                  />
+                  <FormFeedback>User name doesn't exist!</FormFeedback>
+                </FormGroup>
+                <FormGroup>
+                  <Label for='userPassword'>Password</Label>
+                  <Input
+                    type='password' name='userPassword' id='userPassword' placeholder='Your password'
+                    invalid={this.state.userPasswordError}
+                    onChange={(e) => this.setState({ userPassword: e.target.value })}
+                  />
+                  <FormFeedback>Wrong password!</FormFeedback>
+                </FormGroup>
+              </Form>
+            </div>
+          </ModalBody>
           <ModalFooter>
-            <Button onClick={this.toggle}>Login</Button>
+            <Button onClick={this.login}>Login</Button>
             <Button onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
