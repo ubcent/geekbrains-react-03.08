46d777141b52efbe266e6b60746a6ae4c4ebaf19
Никита Сысоев <na.sysoev@gmail.com>
2018-08-13 21:39:01 +0300

    lesson_3 is done

--- Nikita_Sysoev/src/components/App.jsx
+++ Nikita_Sysoev/src/components/App.jsx
@@ -1,9 +1,19 @@
+import './App.css';
+
 import React, {Component} from "react";
+
+import Header from "./Header";
+import Content from "./Content/Content";
+import Footer from "./Footer/Footer";
 
 export default class App extends Component {
     render() {
-        return <h1>
-            Nikita Sysoevs
-        </h1>
+        return (
+            <div className='app'>
+                <Header/>
+                <Content/>
+                <Footer/>
+            </div>
+        )
     }
 }
---
+++ Nikita_Sysoev/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+import Wrapper from "../Wrapper";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <Wrapper>
+                    All right reserved
+                </Wrapper>
+            </footer>
+        )
+    }
+}
