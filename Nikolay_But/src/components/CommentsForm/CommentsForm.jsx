7b2fb0c6d17d7169c372ed2fef7efa1b15332719
mflow42 <but.nikolay@gmail.com>
2018-08-17 01:04:04 +0300

    sync with upstream git

---
+++ Nikolay_But/src/components/CommentsForm/CommentsForm.jsx
@@ -0,0 +1,17 @@
+import './CommentsForm.scss';
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CommentsForm extends Component {
+  static propTypes = {}
+
+  static defaultProps = {}
+
+  render() {
+    return (
+      <div className="CommentsForm"></div>
+    );
+  }
+}
+  
---
+++ Nikolay_But/src/components/CommentsList/CommentsList.jsx
@@ -0,0 +1,17 @@
+import './CommentsList.scss';
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class CommentsList extends Component {
+  static propTypes = {}
+
+  static defaultProps = {}
+
+  render() {
+    return (
+      <div className="CommentsList"></div>
+    );
+  }
+}
+  
