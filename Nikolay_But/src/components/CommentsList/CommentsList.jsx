c057d26ca989cad9baa19c691ea9174bb08fe5ab
mflow42 <but.nikolay@gmail.com>
2018-08-17 01:56:39 +0300

    lesson 4 home work done

--- Nikolay_But/src/components/CommentsList/CommentsList.jsx
+++ Nikolay_But/src/components/CommentsList/CommentsList.jsx
@@ -3,15 +3,30 @@ import './CommentsList.scss';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class CommentsList extends Component {
-  static propTypes = {}
+import Comment from 'components/Comment';
 
-  static defaultProps = {}
+export default class CommentsList extends Component {
+
+  static propTypes = {
+    comments: PropTypes.arrayOf(
+      PropTypes.shape({
+        author: PropTypes.string.isRequired,
+        message: PropTypes.string.isRequired,
+      })
+    )
+  }
+
+  static defaultProps = {
+    comments: [],
+  }
 
   render() {
+    const { comments } = this.props;
+
     return (
-      <div className="CommentsList"></div>
+      <ul className="CommentsList">
+        {comments.map((comment) => <li><Comment {...comment}/></li>)}
+      </ul>
     );
   }
 }
-  
