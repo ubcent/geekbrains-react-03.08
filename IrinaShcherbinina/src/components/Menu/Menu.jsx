c05e59f2010d06112d2e2fc06f85506f67120cd9
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-08-21 10:58:40 +0300

    initial commit

--- IrinaShcherbinina/src/components/Menu/Menu.jsx
+++ IrinaShcherbinina/src/components/Menu/Menu.jsx
@@ -1,32 +1,40 @@
-import './Menu.css';
+import './menu.css';
 
+import classNames from 'classnames'; 
 import React, { Component } from 'react';
-import classNames from 'classnames';
+
+//помогает проверять входные параметры
 import PropTypes from 'prop-types';
 
+
+
 export default class Menu extends Component {
-  static propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({ href: PropTypes.string, label: PropTypes.string })
-    ),
-    size: PropTypes.oneOf(['big', 'small']),
-  }
+	//описываем параметры, которые используем
+	static propTypes = {
+		//входное prop items  - это массив объектов со свойствами определенных типов
+		items: PropTypes.arrayOf(
+		PropTypes.shape({href: PropTypes.string, label:PropTypes.string})
+		),
+		size: PropTypes.oneOf(['big', 'small']),
+	}
 
-  static defaultProps = {
-    size: 'small',
-    items: []
-  }
+//как решить проблему с пустым свойством
+static defaultProp ={
+	size: 'small',
+	items: []
+}
 
-  render() {
-    const { items, size } = this.props;
-    
-    const className = classNames('menu', {
-      'menu-big': size === 'big',
-      'menu-small': size === 'small',
-    });
-
-    return (
-      <ul className={className}>{items.map((item) => <li><a href={item.href}>{item.label}</a></li>)}</ul>
-    );
-  }
+	render() {
+		const { items, size } = this.props;
+		
+		const className = classNames('menu', {
+			'menu-big': size === 'big',
+			'menu-small': size === 'small',
+		})
+		
+		return (
+			<ul className={classNames}>{items.map((item) => <li><a href={item.href}>{item.label}</a></li>)}</ul>
+			
+		);
+}
 }
