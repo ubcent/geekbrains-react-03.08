5844d65f63f3701d5c924b04b14f096e2e7ce7ad
ampleev0503 <ampleev0503@yandex.ru>
2018-08-24 15:40:45 +0300

    Ampleev Vadim lesson 5

--- Ampleev_Vadim/src/components/CommentsList/CommentsList.jsx
+++ Ampleev_Vadim/src/components/CommentsList/CommentsList.jsx
@@ -1,11 +1,11 @@
 import './CommentsList.scss';
 
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import Comment from 'components/Comment';
 
-export default class CommentsList extends Component {
+export default class CommentsList extends PureComponent {
     static propTypes = {
         comments: PropTypes.arrayOf(
             PropTypes.shape({
