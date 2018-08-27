2f1bcd0aaa1bc0daaf69157c4002aa09bdb737ba
greforce <greforce76@gmail.com>
2018-08-27 12:05:48 +0300

    lesson7 - try tests with jest

--- Gregory_Kovorotny/src/components/PostComment/PostComment.test.js
+++ Gregory_Kovorotny/src/components/PostComment/PostComment.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import {shallow, configure} from 'enzyme';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 import PostComment from './PostComment';
 
-test('PostComment should ...', () => {
+test('PostComment should render author and message', () => {
+  const postComment = shallow(<PostComment id={777} postId={22} author="Vasya" message="Hello" />);
 
+  expect(postComment.find('h4').text()).toEqual('Vasya');
+  expect(postComment.find('div.post-comment > div').text()).toEqual('Hello');
 });
--- Gregory_Kovorotny/src/components/PostCommentForm/PostCommentForm.test.js
+++ Gregory_Kovorotny/src/components/PostCommentForm/PostCommentForm.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import {shallow, configure} from 'enzyme';
 
-configure({ adapter: new Adapter() });
+configure({adapter: new Adapter()});
 
 import PostCommentForm from './PostCommentForm';
+import {
+  Button,
+} from 'reactstrap';
 
-test('PostCommentForm should ...', () => {
+test('PostCommentForm should fire onSubmit event', () => {
+  const mockOnSubmit = jest.fn();
+  const mockPreventDefault = jest.fn();
+  const mockObject = {
+    author: 'Vasya',
+    message: 'Message',
+    postId: 22,
+  };
+  const postComment = shallow(<PostCommentForm postId={22} onSubmit={mockOnSubmit} />); // проверить в тесте postId
 
+  postComment.setState(mockObject);
+  postComment.find(Button).simulate('click', {preventDefault: mockPreventDefault});
+
+  expect(mockOnSubmit.mock.calls[0][0]).toEqual(mockObject);
+  expect(mockPreventDefault.mock.calls.length).toBe(1);
 });
