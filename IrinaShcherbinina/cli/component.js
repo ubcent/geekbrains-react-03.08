361734c5df34ef87388c5a5df24d9a701520f52e
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-08-31 16:23:17 +0300

    убрала комментарии и лишние строки

--- IrinaShcherbinina/cli/component.js
+++ IrinaShcherbinina/cli/component.js
@@ -1,32 +1,22 @@
-//встроенная библиотека node для работы с файлами
+
 const fs = require('fs');
 const path = require('path');
 const args = require('minimist')(process.argv.slice(2), { alias: {name: 'n'}});
-	
-
 
 console.log(args);														 
-														 
 const {name} = args;
-//Первое - создаем папку
+
 fs.mkdirSync(path.resolve(__dirname, '..', 'src', 'components', name));
 
-//Сгенерируем файл с компонентом .jsx
 fs.writeFileSync(path.resolve(__dirname, '..', 'src', 'components', name, `${name}.jsx`),
 				 `import './${name}.scss';
 
-
 import React, { Component } from 'react';
-
-//помогает проверять входные параметры
 import PropTypes from 'prop-types';
 export default class ${name} extends Component {
-	//описываем параметры, которые используем
 	static propTypes = {}
 
-//как решить проблему с пустым свойством
 static defaultProp ={}
-
 	render() {
 		return (
 			<div className="${name}"></div>
@@ -36,28 +26,10 @@ static defaultProp ={}
 `,
 );
 
-//Сгенерируем стили
 fs.writeFileSync(path.resolve(__dirname, '..', 'src', 'components', name, `${name}.scss`),
 `.${name} {}`,
 );
 
-//Сгенерируем index.js
 fs.writeFileSync(path.resolve(__dirname, '..', 'src', 'components', name, 'index.js'),
 `export default from './${name}';`,
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
