6eb1cf6b3117ed4af66dcd27f46504ddb6119a06
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-08-31 16:30:49 +0300

    убрала комментарии и лишние строки

--- IrinaShcherbinina/src/components/Comment/Comment.jsx
+++ IrinaShcherbinina/src/components/Comment/Comment.jsx
@@ -1,17 +1,13 @@
 import './Comment.scss';
 
-
 import React, { Component } from 'react';
 
-//помогает проверять входные параметры
 import PropTypes from 'prop-types';
 export default class Comment extends Component {
-	//описываем параметры, которые используем
 	static propTypes = {
-		author: PropTypes.string.isRequired,
+	 	author: PropTypes.string.isRequired,
 		message: PropTypes.string.isRequired,
 	}
-
 	render() {
 		const {author, message} = this.props;
 		
