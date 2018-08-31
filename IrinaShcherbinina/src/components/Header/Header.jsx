ba8a2b8a1d103ddde1e5202e19bfb6733b7b8c2b
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-08-31 16:44:30 +0300

    убрала комментарии и лишние строки

--- IrinaShcherbinina/src/components/Header/Header.jsx
+++ IrinaShcherbinina/src/components/Header/Header.jsx
@@ -1,19 +1,13 @@
 import './Header.scss';
 
-
 import React, { Component } from 'react';
-
-//помогает проверять входные параметры
 import PropTypes from 'prop-types';
-
 import {withRouter} from 'react-router-dom';
 
 class Header extends Component {
-	//описываем параметры, которые используем
 	static propTypes = {}
 
-//как решить проблему с пустым свойством
-static defaultProp ={}
+	static defaultProp ={}
 
 	render() {
 		const {match} = this.props;
