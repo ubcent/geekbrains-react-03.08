a05e4bcc0292081e52ca7d245bc001b02f433a41
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-08-31 16:45:38 +0300

    убрала комментарии и лишние строки

--- IrinaShcherbinina/src/components/HomePage/HomePage.jsx
+++ IrinaShcherbinina/src/components/HomePage/HomePage.jsx
@@ -1,17 +1,13 @@
 import './HomePage.scss';
 
-
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
-//помогает проверять входные параметры
 import PropTypes from 'prop-types';
+
 export default class HomePage extends Component {
-	//описываем параметры, которые используем
 	static propTypes = {}
 
-//как решить проблему с пустым свойством
-static defaultProp ={}
+	static defaultProp ={}
 
 	render() {
 		return (
