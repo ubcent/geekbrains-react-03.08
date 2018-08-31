408aaf9d1dbac2ba14a8afe0e9f11d8b00a4d503
IrinaShcherbinina <42144602+IrinaShcherbinina@users.noreply.github.com>
2018-08-31 16:47:31 +0300

    убрала комментарии и лишние строки

--- IrinaShcherbinina/src/components/Test/Test.jsx
+++ IrinaShcherbinina/src/components/Test/Test.jsx
@@ -1,16 +1,12 @@
 import './Test.scss';
 
-
 import React, { Component } from 'react';
-
-//помогает проверять входные параметры
 import PropTypes from 'prop-types';
+
 export default class Test extends Component {
-	//описываем параметры, которые используем
 	static propTypes = {}
 
-//как решить проблему с пустым свойством
-static defaultProp ={}
+	static defaultProp ={}
 
 	render() {
 		return (
